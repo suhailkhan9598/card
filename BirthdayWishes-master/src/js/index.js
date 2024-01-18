676d3c9e90d296a7d0b5a45b146fe8be949b242b
suhail khan <khansuhail9598@gmail.com>
2024-01-18 12:03:15 +0530

    update birthday

--- BirthdayWishes-master/src/js/index.js
+++ BirthdayWishes-master/src/js/index.js
@@ -1,17 +1,17 @@
 
-let firstSlideContainer = 
+let firstSlideContainer =
 document.getElementsByClassName("slide--content")[0];
 
-let secondSlideContainer = 
+let secondSlideContainer =
 document.getElementsByClassName("slide--content--one")[0];
 
-let secondCanvas = 
+let secondCanvas =
 document.getElementsByClassName("second--canvas")[0];
 
 secondSlideContainer.setAttribute("style","display:none");
 secondCanvas.setAttribute("style","display:none");
 
-let thirdCanvas = 
+let thirdCanvas =
 document.getElementsByClassName("third--canvas")[0];
 
 thirdCanvas.setAttribute("style","display:none");
@@ -21,7 +21,7 @@ thirdCanvas.setAttribute("style","display:none");
 let containerToggleOne = setTimeout(function(){
 	firstSlideContainer.setAttribute("style","display:none");
 	secondSlideContainer.setAttribute("style","display:block");
-},2500);
+}, 4000);
 
 
 let removeFirstSlide = setTimeout(function(){
